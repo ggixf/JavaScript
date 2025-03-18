6d4e69058b96fec08ac95d71e659ef97d1119232
ggixf <giovanna.fogaca068@al.unieduk.com.br>
2025-03-18 20:29:59 -0300

    alteração aula 1805

--- React1.js
+++ React1.js
@@ -5,7 +5,7 @@ export default function App(){
   return (
     <View>
      <Text>Olá, Turma!</Text>
-     <Text>Quero ler os meus livros!!</Text>
+     <Text>Quero ler os meus livros!!!!!!</Text>
      </View>
   );
 }
