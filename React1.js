ecd5887a235bea24f43b806991b5d5e3c71fb632
ggixf <giovanna.fogaca068@al.unieduk.com.br>
2025-03-18 20:27:47 -0300

    180325

---
+++ React1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import{Text, View} from 'react-native';
+
+export default function App(){
+  return (
+    <View>
+     <Text>Olá, Turma!</Text>
+     <Text>Quero ler os meus livros!!</Text>
+     </View>
+  );
+}
